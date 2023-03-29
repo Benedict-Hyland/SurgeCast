cac4b6dff9b38caccb3ff30a5f37596ffb34f8b0
Benedict-Hyland <benedicthyland@gmail.com>
2023-03-29 18:19:08 +0100

    End of day commit 29-03-23

--- src/data/credentials.py
+++ src/data/credentials.py
@@ -1,20 +1,49 @@
 import configparser
 import os
+import pymysql
+import pymysql.cursors
+import pandas as pd
 
-def get_mysql_configs():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
+def get_file():
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(os.getcwd()), '../'))
+    config_file = os.path.join(base_dir, 'config' ,'database_credentials.ini')
+    return base_dir, config_file
 
-    base_dir = os.path.abspath(os.path.join(script_dir, '../../'))
+def get_configs(file, section='test'):
 
     config = configparser.ConfigParser()
-    config.read(os.path.join(base_dir, '/config/database_credentials.ini'))
+    config.read(file)
 
-    host = config.get('mysql', 'host')
-    database = config.get('mysql', 'database')
-    user = config.get('mysql', 'user')
-    password = config.get('mysql', 'password')
+    host = config.get(section, 'host')
+    database = config.get(section, 'database')
+    user = config.get(section, 'user')
+    password = config.get(section, 'password')
 
     return host, database, user, password
 
+def run_query(host, database, user, password, query):
+    # Connect to the database
+    connection = pymysql.connect(host=host,
+                                 user=user,
+                                 password=password,
+                                 database=database,
+                                 cursorclass=pymysql.cursors.DictCursor)
+    
+    try:
+        df=pd.DataFrame(columns=['DateTime','Postcode','CC','CP','GST','HiT','HL','LoT','MWD','MWS','NCP','SD','SD_P','SwD','SwF','T','TP','Vs'])
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            
+            # Fetch and store the results in pandas dataframe in chunks
+            while True:
+                chunk = cursor.fetchmany(size=1000)
+                if not chunk:
+                    break
+                df = df.append(chunk)
+    finally:
+        connection.close()
+    return df
+
 if __name__ == '__main__':
-    host, database, user, password = get_mysql_configs()
+    base_dir, config_file = get_file()
+    host, database, user, password = get_configs(file=config_file, section='test')
